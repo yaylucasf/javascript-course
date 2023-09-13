dba6c593c22e3d3f97b77d8649ce641df5b0ad41
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-13 17:02:08 -0300

    Studying about the 'Math' object

---
+++ data-types/math/index.js
@@ -0,0 +1,31 @@
+const x = Math.ceil(3.5);
+
+console.log(x);
+
+const y = Math.floor(3.5);
+
+console.log(y);
+
+const z = Math.round(3.5);
+
+console.log(z);
+
+const a = Math.abs(-15);
+
+console.log(a);
+
+const b = Math.sqrt(25);
+
+console.log(b);
+
+const c = Math.pow(2, 3);
+
+console.log(c);
+
+const d = Math.max(3, 7, 0, 1, 4, 5);
+
+console.log(`Max: ${d}`);
+
+const e = Math.min(3, 7, 0, 1, 4, 5);
+
+console.log(`Min: ${e}`);
