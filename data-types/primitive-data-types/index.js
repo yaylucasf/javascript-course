86efcb7a69d3d28b559d1b2af03a7f4644a05f20
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-13 16:55:12 -0300

    Studying about data types

---
+++ data-types/primitive-data-types/index.js
@@ -0,0 +1,9 @@
+const x = 5;
+const y = 3.5;
+const z = true;
+const a = 'ABC';
+
+console.log(typeof x);  // number
+console.log(typeof y);  // number 
+console.log(typeof z);  // boolean
+console.log(typeof a);  // string
