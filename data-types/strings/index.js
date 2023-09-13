fb9af06e077ed6845004e2d85c14b1f00787e5b9
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-13 18:28:44 -0300

    Studying about strings

---
+++ data-types/strings/index.js
@@ -0,0 +1,19 @@
+let a = 'JavaScript';
+
+console.log(a);
+
+let b = 'Java\nScript';
+
+console.log(b);
+
+let c = 'Java\tScript';
+
+console.log(c);
+
+let d = 'Java"S"cript';
+
+console.log(d);
+
+let e = 'Java\'S\'cript';
+
+console.log(e);
