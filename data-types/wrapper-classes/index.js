768a3d269bb6692858798dbc30a6ba8623eba5ca
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-14 16:43:30 -0300

    Studying about wrapper classes

---
+++ data-types/wrapper-classes/index.js
@@ -0,0 +1,6 @@
+let x = "JavaScript";  // string
+let y = String(x).toUpperCase();  // String(x).toUpperCase();
+
+console.log(y);
+
+// Wrapper Classes: (number -> Number), (string -> String), (boolean -> Boolean)
