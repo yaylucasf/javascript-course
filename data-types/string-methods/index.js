fd93eb4538ed1c5cb6d4940ccf39c46e693faf67
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-14 13:50:43 -0300

    Studying about strings methods

---
+++ data-types/string-methods/index.js
@@ -0,0 +1,9 @@
+const lang = 'JavaScript';
+
+console.log(lang.slice(4));
+
+console.log(lang.toUpperCase());
+console.log(lang.toLowerCase());
+console.log(`Length: ${lang.length}`);
+console.log(`Index of 'a': ${lang.indexOf('a')}`);
+console.log(`Last index of 'a': ${lang.lastIndexOf('a')}`);
